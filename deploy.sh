03c4747670ae286c87519299595c8c91f8d3f6b2
Josh <joshconstantinelim@gmail.com>
2022-08-19 22:39:50 +1000

    update deploy to deploy ,.data too

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ WORKING_DIRECTORY="~/www/ibexpertdeploy"
 USERNAME="ibexpert"
 SSH_HOST="ssh-ibexpert.alwaysdata.net"
 
-scp -r ./package.json ./package-lock.json ./tsconfig.json ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
+scp -r ./data ./package.json ./package-lock.json ./tsconfig.json ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
 ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && npm install --only=production"
