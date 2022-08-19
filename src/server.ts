1c9c213c585e74d898010ea702a96ba719522615
Josh <joshconstantinelim@gmail.com>
2022-08-19 18:57:39 +1000

    updating server for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from "express";
+
+import config from "./config.json";
 const app = express();
-const port = 3000;
+
+const PORT: number = parseInt(process.env.PORT || config.port);
+const HOST: string = process.env.IP || "localhost";
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ response: "IB Expert Backend" });
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+app.listen(parseInt(process.env.PORT || config.port), process.env.IP, () => {
+  console.log(
+    `⚡️ Server listening on port ${process.env.PORT || config.port}`
+  );
 });
