4c731497a8eb61ee140579e13d8e4597eeb61984
Josh <joshconstantinelim@gmail.com>
2022-08-22 20:56:50 +1000

    tests for /home/ functions

--- tests/unit_tests/home.test.ts
+++ tests/unit_tests/home.test.ts
@@ -1,5 +1,38 @@
 import { homeTutors, homeSubjects, homeFAQs } from "../../src/home";
 
-test("expect", () => {
-  expect(1 + 1).toBe(2);
+test("homeTutors() returns correct type", () => {
+  const res = homeTutors();
+  res.tutors.forEach((tutor) => {
+    expect(tutor).toStrictEqual({
+      name: expect.any(String),
+      mark: expect.any(Number),
+      bio: expect.any(String),
+      picURL: expect.any(String),
+    });
+  });
+});
+
+test("homeSubjects() returns correct type", () => {
+  const res = homeSubjects();
+
+  res.subjects.forEach((group) => {
+    group.forEach((subject) => {
+      expect(typeof subject.name).toBe("string");
+      expect(subject.name.length).toBeGreaterThan(0);
+
+      expect(typeof subject.level).toBe("number");
+      expect(subject.level === 1 || subject.level === 0).toBeTruthy();
+    });
+  });
+});
+
+test("homeFAQs() returns correct type", () => {
+  const res = homeFAQs();
+
+  res.faqs.forEach((qn) => {
+    expect(qn).toStrictEqual({
+      question: expect.any(String),
+      answer: expect.any(String),
+    });
+  });
 });
